04f55b596313cbaf35b20b167a6ac5ea6f676b71
ianspehar99 <ian.spehar01@gmail.com>
2025-02-01 02:07:52 +0000

    h

--- picarx/sensor_classes_w3.py
+++ picarx/sensor_classes_w3.py
@@ -15,7 +15,7 @@ class SENSOR():
 
 class INTERP():
 
-    def __init__(self, px,sensitivity = 300, polarity = 1, reference = [500,500,500]):
+    def __init__(self, px,sensitivity = 400, polarity = 1, reference = [500,500,500]):
         #If line is darker than background, use default polarity
         #If line is lighter, set polarity to 0
         self.px = px
@@ -40,31 +40,31 @@ class INTERP():
         dif1 = Middle - Left
         dif2 = Right - Middle
         position = None
-        if abs(dif1) > sensitivity: 
-            if abs(dif2) > sensitivity:
-                print("Car is centered")
-                position = 0
-            if abs(dif2) < sensitivity:
-                #Car is either on edge case to the right, or is left of center
-                if dif1 < 0:
-                    print("car is to the left")
-                    position = -1*(dif1/sensitivity)
-                elif dif1 > 0:
-                    print("car at edge case to the right")
-                    position = 0.5*(dif1/sensitivity)
-        elif abs(dif1) < sensitivity:
-            if dif2 > 0:
+        if abs(dif1) > sensitivity and abs(dif2) > sensitivity and dif1 < 0:
+            print("Car is centered")
+            position = 0
+        elif abs(dif1) > sensitivity and abs(dif2) < sensitivity:
+            if dif1 > 0:
                 print("Car is to the right")
-                position = 1*(dif2/sensitivity)
-            elif dif2 < 0:
+                position = 1*(abs(dif1)/sensitivity)
+            elif dif1 < 0:
                 print("Car at edge case to left")
-                position = 0.5*(dif2/sensitivity)
-        return position #hey
+                position = -0.5*(abs(dif1)/sensitivity) 
+        elif abs(dif1) < sensitivity and abs(dif2) > sensitivity:
+            if dif2 > 0:
+                print("car at edge case to the right")
+                position = 0.5*(abs(dif2)/sensitivity)
+            elif dif2 < 0:
+                print("car is to the left")
+                position = -1*(abs(dif2)/sensitivity)
+        else:
+            position = 0
+        return position 
     
 
 class CONTROL():
 
-    def __init__(self,Picar, scale_factor = 10):
+    def __init__(self,Picar, scale_factor = 20):
         #Negative to correct the offset
         self.scale_factor = -1*scale_factor
         self.px = Picar
